1abb0c0efe26328018faa581d46a00e4578e7a2c
Iain McManus <iainmcmanus@protonmail.com>
2024-03-01 10:39:18 +1000

    Initial commit

---
+++ Assets/DynamicUIDisplayScreen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DynamicUIDisplayScreen : MonoBehaviour
+{
+    [SerializeField] LayerMask RaycastMask = ~0;
+    [SerializeField] float RaycastDistance = 5.0f;
+    [SerializeField] UnityEvent<Vector2> OnCursorInput = new();
+
+    // Update is called once per frame
+    void Update()
+    {
+#if ENABLE_LEGACY_INPUT_MANAGER
+        Vector3 MousePosition = Input.mousePosition;
+#else
+        Vector3 MousePosition = UnityEngine.InputSystem.Mouse.current.position.ReadValue();
+#endif // ENABLE_LEGACY_INPUT_MANAGER
+
+        // construct our ray from the mouse position
+        Ray MouseRay = Camera.main.ScreenPointToRay(MousePosition);
+
+        // perform our raycast
+        RaycastHit HitResult;
+        if (Physics.Raycast(MouseRay, out HitResult, RaycastDistance, RaycastMask, QueryTriggerInteraction.Ignore))
+        {
+            // ignore if not us
+            if (HitResult.collider.gameObject != gameObject)
+                return;
+
+            OnCursorInput.Invoke(HitResult.textureCoord);
+        }
+    }
+}
